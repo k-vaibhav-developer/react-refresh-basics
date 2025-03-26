8d65bff42f2a963e480ae7c214e6147bc6e79bf2
k-vaibhav-developer <kvaibhav467@gmail.com>
2025-03-27 05:09:45 +0530

    context api - 2

---
+++ context_api_2/src/App.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+import "./App.css";
+import ChildA from "./components/ChildA";
+
+const ThemeContext = createContext();
+
+const UserContext = createContext();
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  const [user, setUser] = useState({ name: "King" });
+
+  return (
+    <UserContext.Provider value={user}>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <div
+          id="container"
+          style={{ backgroundColor: theme === "light" ? "beige" : "black" }}
+        >
+          <ChildA />
+        </div>
+      </ThemeContext.Provider>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+export { ThemeContext, UserContext };
+
